2312a71e101baa5df9d7da74adbdd1956419065e
Ifrat Ikhtear Uddin <ifrat.ikhtear@brainstation-23.com>
2022-12-31 00:11:23 +0600

    json parse issue fixed

--- src/API/api.ts
+++ src/API/api.ts
@@ -6,45 +6,16 @@ import { QueryObject } from "src/common/Entity/QueryObject";
 import { LoginParams, LoginResponse } from "src/context/types";
 import { commonHelperService } from "src/common/helper/common.helper";
 import { toast } from "react-toastify";
-
-axios.defaults.baseURL = "http://localhost:5000/api/v1";
+import { axiosPrivate } from "src/common/Interceptors/api.interceptor";
 
 const responseBody = <T>(response: AxiosResponse<T>) => response.data;
 
-
-axios.interceptors.response.use(
-  async (response) => {
-    return response;
-  },
-  (error: AxiosError) => {
-    console.log("error")
-    const { data, status, config } = error.response!;
-    switch (status) {
-      case 400:
-        console.log(data);
-        break;
-      case 404:
-        toast.error("server error");
-        break;
-      case 500:
-        toast.error("server error");
-        break;
-
-      default:
-        toast.error("an unknown error occurred");
-        break;
-    }
-    return Promise.reject(error);
-  }
-);
-
-
 const request = {
-  get: <T>(url: string, body : QueryObject) => axios.get<T>(`${url}?${commonHelperService.convertToQueryString(body)}`).then(responseBody),
+  get: <T>(url: string, body: QueryObject) => axiosPrivate.get<T>(`${url}?${commonHelperService.convertToQueryString(body)}`).then(responseBody),
   post: <T>(url: string, body: object) =>
-    axios.post<T>(url, body).then(responseBody),
-  put: <T>(url: string, body: {}) => axios.put<T>(url, body).then(responseBody),
-  delete: <T>(url: string) => axios.delete<T>(url).then(responseBody),
+    axiosPrivate.post<T>(url, body).then(responseBody),
+  put: <T>(url: string, body: {}) => axiosPrivate.put<T>(url, body).then(responseBody),
+  delete: <T>(url: string) => axiosPrivate.delete<T>(url).then(responseBody),
 };
 
 const singleValueTypeConfigApi = {
@@ -55,44 +26,14 @@ const singleValueTypeConfigApi = {
 }
 
 const auth = {
-  login : (loginParam :LoginParams) => request.post<any>(authConfig.loginEndpoint, loginParam),
-  refreshToken : (payload:any) => request.post<any>(authConfig.refreshTokenEndpoint, payload) 
+  login: (loginParam: LoginParams) => request.post<any>(authConfig.loginEndpoint, loginParam),
+  refreshToken: (payload: any) => request.post<any>(authConfig.refreshTokenEndpoint, payload)
 }
 
-/*
-Please follow this example for CRUD operation
 
-const Experience = {
-    list: () => request.get<Experience[]>("/experience"),
-
-    create: (payload:ExperiencePayload) => {
-
-     //if form is not sending file, send the payload directly not need to create FormData object
-     
-      const formData = new FormData();
-      formData.append("Company", payload.company);
-      formData.append("Position", payload.position);
-      formData.append("Responsibilities", payload.responsibilities);
-      formData.append("StartDate", StartDate);
-      if(EndDate) formData.append("EndDate", EndDate);
-      formData.append("PhotoFile", payload.photoFile);
-  
-      return axios.post<any>("/experience", formData, {
-        headers: { "Content-type": "multipart/form-data" },
-      });
-    },
-
-    update:(id:string, payload:ExperiencePayload) => {
-      return axios.put<any>(`/experience/${id}`, payload);
-    },
-
-    delete: (id:string) => axios.delete(`experience/${id}`)
-  };
-
-*/
 const api = {
   singleValueTypeConfigApi,
-  auth  
+  auth
 }
 
 export default api;
